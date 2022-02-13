ef1da5d8ee7e4b485cb579bf8190e74af38e9dea
Sergey Kudryavtsev <sergey.developer@bk.ru>
2022-02-13 13:59:35 +0300

    request data from RUL on load

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,20 +8,29 @@
 import XCTest
 
 class RemoteFeedLoader {
-    
+    func load(){
+        HTTPClient.instance.requestedURL = URL(string: "eree/ee")
+    }
 }
 
 class HTTPClient {
+    static let instance = HTTPClient()
+    private init() {}
     var requestedURL: URL?
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.instance
         _ = RemoteFeedLoader()
         XCTAssertNil(client.requestedURL)
     }
     
-
+    func test_load_requestedDataFromURL() {
+        let client = HTTPClient.instance
+        let sut = RemoteFeedLoader()
+        sut.load()
+        XCTAssertNotNil(client.requestedURL)
+    }
 }
